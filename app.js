bab89283983da16042ef3a499dd6fcfca536446c
Ade Dwi Prayitno <ade@technopartner.co.id>
2024-02-15 14:21:02 +0700

    add: show ejs

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ app.get('/', (req, res) => {
     res.render("home")
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: "My Backyard", description: "Great" });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('campgrounds/:id', async (req, res) => {
+    res.render('campgrounds/show');
 });
